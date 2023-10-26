d36cc2981b188653b0e20eaa615db217d3208e5a
chrisdcs <ding.chi@ufl.edu>
2023-10-26 13:36:58 -0400

    add seeds

---
+++ utils/general.py
@@ -0,0 +1,13 @@
+import os
+import random
+import numpy as np
+import torch
+
+
+def init_seeds(seed=0, deterministic=False):
+    # Initialize random number generator (RNG) seeds https://pytorch.org/docs/stable/notes/randomness.html
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # for Multi-GPU, exception safe
