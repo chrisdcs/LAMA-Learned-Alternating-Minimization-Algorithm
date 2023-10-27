c083dac47a6d694395a9e6b28ac2a56117b95727
chrisdcs <ding.chi@ufl.edu>
2023-10-27 11:22:08 -0400

    process data script

---
+++ process-raw.py
@@ -0,0 +1,11 @@
+import utils.CThelper as CT
+
+datasets = ['mayo', 'NBIA']
+views = [64, 128]
+
+for dataset in datasets:
+    for view in views:
+        CT.down_sample(dataset, view, 'FullViewNoiseless', train=True)
+        CT.down_sample(dataset, view, 'FullViewNoiseless', train=False)
+        CT.fbp_data(dataset, view, train=True)
+        CT.fbp_data(dataset, view, train=False)
