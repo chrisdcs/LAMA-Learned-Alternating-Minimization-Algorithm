b303f81b0ff2abe9ee0b59fa8730089bbf0229b9
chrisdcs <ding.chi@ufl.edu>
2023-11-07 17:06:43 -0500

    process data done

--- process-data.py
+++ process-data.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+import scipy.io as scio
 import utils.CT_helper as CT
 from utils.model import InitNet
 from utils.dataloaders import CNN_loader
@@ -21,52 +22,62 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd())) # relative path to current working directory
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 datasets = ['mayo', 'NBIA']
 views = [64, 128]
 
+def parse_opt():
+    parser = argparse.ArgumentParser(description='Init-Net')
+    parser.add_argument('--dataset', type=str, default='mayo', help='name of dataset')
+    parser.add_argument('--train', type=bool, default=True, help='training data or test data')
+    parser.add_argument('--n_views', type=int, default=64, help='number of views for sparse-view CT')
+    parser.add_argument('--network', type=str, default='CNN', help='initialization network name')
+    parser.add_argument('--save', type=bool, default=True, help='save or not')
+    
+    opt = parser.parse_args()
+    print_args(vars(opt))
+    return opt
 
-def generate_sparse_CT(datasets = ['mayo', 'NBIA'], n_views = [64, 128]):
-    for dataset in datasets:
-        for view in views:
-            CT.down_sample(dataset, view, 'FullViewNoiseless', train=True)
-            CT.down_sample(dataset, view, 'FullViewNoiseless', train=False)
-            CT.fbp_data(dataset, view, train=True)
-            CT.fbp_data(dataset, view, train=False)
-    
-    # print("Sparse-View CT data generation Done!")
-    LOGGER.info("Sparse-View CT data generation Done!\n")
-    
-def run(
-        device='',
-        network='',
-        n_views=64,
-        dataset='mayo'
-        ):
+def generate_sparse_CT(dataset, n_views, train=True):
+    LOGGER.info("Sparse-View CT data generation start!")
+    CT.down_sample(dataset, n_views, 'FullViewNoiseless', train=train)
+    CT.fbp_data(dataset, n_views, train=train)
+    LOGGER.info("Sparse-View CT data generation complete!\n")
+
+def initNet_data(dataset, n_views, network, train=True):
+    # will add code for different networks later
     model = InitNet()
     model = nn.DataParallel(model)
     model = model.to(device)
     weights = ROOT / 'models' / 'initNet' / f'{dataset}{n_views}-{network}.pkl'
     model.load_state_dict(torch.load(weights))
-    
+    mode = 'train' if train else 'test'
+    save_dir = ROOT / 'dataset' / dataset / mode / f'{network}{n_views}views'
+    if save_dir.exists():
+        LOGGER.info(f"Init-Net data generation ({dataset} {mode} {n_views}views {network}) already exists!")
+        LOGGER.info(f"Init-Net data generation ({dataset} {mode} {n_views}views {network}) complete!\n")
+        return
+    save_dir.mkdir(parents=True)
     mask = CT.generate_mask()
     ct_cfg = CT.load_CT_config(ROOT / 'config' / '512views.yaml')
     ct_cfg_full = CT.load_CT_config(ROOT / 'config' / 'full-view.yaml')
-    dataset = DataLoader(CNN_loader(ROOT, dataset, n_views, train=True), batch_size=1, shuffle=False, num_workers=8)
+    data_loader = DataLoader(CNN_loader(ROOT, dataset, n_views, train=train), batch_size=1, shuffle=False, num_workers=8)
     
+    LOGGER.info(f"Init-Net data generation ({dataset} {mode} {n_views}views {network}) start!")
     with torch.no_grad():
         avg_p = 0
         avg_s = 0
-        for i, data in enumerate(dataset):
+        for i, data in enumerate(data_loader):
             x, label, file_name = data
             x = x.to(device)
             label = label.to(device)
             output = model(x)
-            # output = output.squeeze().cpu().numpy()
-            # scio.savemat(ROOT / 'dataset' / dataset / 'train' / f'{n_views}views' / file_name, {'data': output})
             
             img = ctlib.fbp(output / 3.84, ct_cfg)
             img = img.detach().cpu().squeeze().numpy()
             img = img.clip(0,1) * mask
+            
+            scio.savemat(save_dir / file_name[0], {'data': img})
             
             img_label = ctlib.fbp(label / 3.84, ct_cfg_full)
             img_label = img_label.squeeze().cpu().numpy()
@@ -78,25 +89,23 @@ def run(
             avg_p += p
             avg_s += s
             
-            LOGGER.info(f'{file_name[0]}\tpsnr {p:.3f}\tssim {s:.3f}')
-        LOGGER.info(f'avg_psnr {avg_p / (i+1):.3f}\tavg_ssim {avg_s / (i+1):.3f}')
+            # LOGGER.info(f'{file_name[0]}\tpsnr {p:.3f}\tssim {s:.3f}')
+        LOGGER.info(f"avg_psnr {avg_p / (i+1):.3f}\tavg_ssim {avg_s / (i+1):.3f}")
+        LOGGER.info(f"Init-Net data generation ({dataset} {mode} {n_views}views {network}) complete!\n")
 
-def parse_opt():
-    parser = argparse.ArgumentParser(description='Init-Net')
-    parser.add_argument('--dataset', type=str, default='mayo', help='dataset name')
-    parser.add_argument('--n_views', type=int, default=64, help='number of views')
-    parser.add_argument('--device', type=str, default='cuda', help='device')
-    parser.add_argument('--network', type=str, default='CNN', help='initialization network name')
+def run(**kwargs):
+    dataset = kwargs['dataset']
+    n_views = kwargs['n_views']
+    train = kwargs['train']
+    network = kwargs['network']
     
-    opt = parser.parse_args()
-    print_args(vars(opt))
-    return opt
+    generate_sparse_CT(dataset, n_views, train=train)
+    initNet_data(dataset, n_views, network, train=train)
     
     
 
 def main():
     opt = parse_opt()
-    generate_sparse_CT()
     run(**vars(opt))
 
 if __name__ == '__main__':
